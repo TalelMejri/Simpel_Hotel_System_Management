c0ddad49b83c17e40317a3d2ffa41ea37cfce341
talel <talelmejri8@gmail.com>
2022-08-23 15:04:19 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/dist/offcanvas"
 Vue.config.productionTip = false
 
 new Vue({

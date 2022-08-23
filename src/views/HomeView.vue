eee9dba2f78f1fee6dc560c5ec8ec24fbfbce5c9
talel <talelmejri8@gmail.com>
2022-08-23 15:04:05 +0100

    add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <homepage></homepage>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import homepage from "@/components/homepage.vue"
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{
+    homepage
   }
+ 
 }
 </script>
+
+<style scoped>
+ 
+ 
+</style>

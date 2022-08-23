6e97a11fea1c5b854f46e86ce7e7febe91c751f4
talel <talelmejri8@gmail.com>
2022-08-23 17:24:32 +0100

    afficher tous les hotels

--- src/components/DisaplayHotel.vue
+++ src/components/DisaplayHotel.vue
@@ -1,14 +1,58 @@
 <template>
   <div class="py-5 mt-5" id="hotel">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, animi?
+    <div class="card border-left-red shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row align-items-center">
+          <div class="col mr-2">
+            <div
+              class="text-xs font-weight-bold text-primary text-uppercase mb-1"
+            >
+              nombre of all the books
+            </div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">dddd</div>
+          </div>
+          <div class="col-auto"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "DisaplayHotel",
+  props: {
+    hotels: Array,
+  },
+  data() {
+    return {
+      select: 1,
+    };
+  },
+  methods: {
+    updateselect(index) {
+      this.select = index;
+    },
+  },
+  computed: {
+    image() {
+      return this.hotels[this.select].image;
+    },
+    name() {
+      return this.hotels[this.select].name;
+    },
+    star() {
+      return this.hotels[this.select].star;
+    },
+  },
 };
 </script>
 
 <style scoped>
+#hotel {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <DisaplayHotel></DisaplayHotel>
+    <DisaplayHotel :hotels="hotels"></DisaplayHotel>
   </div>
 </template>
 
@@ -10,6 +10,42 @@ export default {
   name: "AboutView",
   components: {
     DisaplayHotel,
+  },
+  data() {
+    return {
+      hotels: [
+        {
+          id: 0,
+          name: "hamamet",
+          star: 4,
+          image: "../assets/logo.png",
+        },
+        {
+          id: 1,
+          name: "gammaret",
+          star: 4,
+          chambre: 50,
+          nbr_valide: 11,
+          image: "../assets/logo.png",
+        },
+        {
+          id: 2,
+          name: "tbarka",
+          star: 4,
+          chambre: 20,
+          nbr_valide: 11,
+          image: "../assets/logo.png",
+        },
+        {
+          id: 3,
+          name: "sousse",
+          star: 4,
+          chambre: 20,
+          nbr_valide: 0,
+          image: "../assets/logo.png",
+        },
+      ],
+    };
   },
 };
 </script>

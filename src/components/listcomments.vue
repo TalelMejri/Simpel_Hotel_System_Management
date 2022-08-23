df6c88a2affd5194c34bb027ea3cc6f944e13d20
talel <talelmejri8@gmail.com>
2022-08-23 16:44:06 +0100

    disabled buuton

--- src/components/listcomments.vue
+++ src/components/listcomments.vue
@@ -1,42 +1,63 @@
 <template>
-<div id="comments">
-    <div class="fw-bolder"  v-if="tabcomments==''"> No comments </div>
-    <div v-else >
-<button class="btn btn-outline-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">All comments</button>
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel">Comments </h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  <div id="comments">
+    <button
+      class="btn btn-outline-primary"
+      :disabled="tabcomments == ''"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasRight"
+      aria-controls="offcanvasRight"
+    >
+      All comments
+    </button>
+    <div
+      class="offcanvas offcanvas-end"
+      tabindex="-1"
+      id="offcanvasRight"
+      aria-labelledby="offcanvasRightLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasRightLabel">Comments</h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body">
+        <div
+          class="desc mb-4"
+          v-for="tabcomment in tabcomments"
+          :key="tabcomment.id"
+        >
+          <div class="details card shadow p-2 text-center">
+            <p>
+              <muted>{{ tabcomment.name }}</muted>
+              <br />
+              <a href="#">{{ tabcomment.email }}</a> <br />
+              {{ tabcomment.message }}<br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="offcanvas-body">
-          <div class="desc mb-4 " v-for="tabcomment in tabcomments" :key="tabcomment.id">
-              <div class="details card shadow p-2 text-center">
-                 <p>
-                  <muted>{{tabcomment.name}}</muted>
-                  <br/>
-                  <a href="#">{{tabcomment.email}}</a> <br> {{tabcomment.message}}<br/>
-                </p>
-              </div>
-            </div>
-  </div>
-</div>
-</div>
-   </div>
 </template>
 
 <script>
- export default{
-    name:'listcomments',
-    props:{
-        tabcomments:Array
-    }
- }
+export default {
+  name: "listcomments",
+  props: {
+    tabcomments: Array,
+  },
+};
 </script>
 
 <style scoped>
-#comments{
-    position: absolute;
-    margin-top: -44rem;
-    margin-left: 46rem;
+#comments {
+  position: absolute;
+  margin-top: -44rem;
+  margin-left: 46rem;
 }
 </style>

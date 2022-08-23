140b768bf2513e616152a3ed876d0f1a89a89358
talel <talelmejri8@gmail.com>
2022-08-23 16:49:10 +0100

    add disaplyhotel components

---
+++ src/components/DisaplayHotel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="py-5 mt-5" id="hotel">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, animi?
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisaplayHotel",
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-      
+    <DisaplayHotel></DisaplayHotel>
   </div>
 </template>
+
+<script>
+import DisaplayHotel from "@/components/DisaplayHotel.vue";
+export default {
+  name: "AboutView",
+  components: {
+    DisaplayHotel,
+  },
+};
+</script>

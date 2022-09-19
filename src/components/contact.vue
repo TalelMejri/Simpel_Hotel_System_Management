223b07935b39557c470113630c6383a3a19c3058
talel <talelmejri8@gmail.com>
2022-09-19 20:25:57 +0100

    add key frames

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -104,7 +104,7 @@ export default {
 
 <style scoped>
 
-.fade-enter-from{
+/*.fade-enter-from{
   opacity:0;
   transform: translateY(-60px);
 }
@@ -112,10 +112,11 @@ export default {
 .fade-enter-to{
   opacity:1;
   transform: translateY(0);
-}
+}*/
 
 .fade-enter-active{
-  transition: all 0.3s ease;
+  /*transition: all 0.3s ease;*/
+  transition: animate 0.8s ease;
 }
 
 .fade-leave-from{
@@ -130,6 +131,15 @@ export default {
 
 .fade-leave-active{
   transition: all 0.3s ease;
+}
+@keyframes animate {
+  0%{opacity: 0;transform: translateY(-60px);}
+  50%{opacity: 1;transform: translateY(0);}
+  60%{transform:translateX(8px)}
+  70%{transform:translateX(-8px)}
+  80%{transform:translateX(4px)}
+  90%{transform:translateX(-4px)}
+  100%{transform:translateX(0)}
 }
 
 </style>

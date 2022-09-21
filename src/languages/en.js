45f0afadaff0ee8a62ce668fac4e55659da34315
talel <talelmejri8@gmail.com>
2022-09-21 15:09:45 +0100

    translate en

---
+++ src/languages/en.js
@@ -0,0 +1,13 @@
+export default{
+    data() {
+        return {
+            en:{
+                Myhotel:"My Hotel",
+                home:"home",
+                about:"store",
+                contact:"contact",
+                allteam:"all team",
+            }
+        }
+    },
+}
